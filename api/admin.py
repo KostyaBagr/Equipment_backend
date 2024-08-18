8204874439d3d9ad1dde86f1737732009d26d937
KostyaBagr <kostabagrov@gmail.com>
2024-08-18 13:54:27 +0300

    created: schemas and admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,21 @@
+
+"""
+Отображение моделей оборудования.
+    EquipmentAdmin: класс для отображения сущности Equipment.
+    EquipmentTypeAdmin: класс для отображения сущности EquipmentType.
+"""
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Equipment, EquipmentType
+
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    """Отображение таблицы Equipment."""
+    pass 
+
+
+@admin.register(EquipmentType)
+class EquipmentTypeAdmin(admin.ModelAdmin):
+    """Отображение таблицы EquipmentType."""
+    pass 
--- api/models.py
+++ api/models.py
@@ -1,3 +1,41 @@
+"""
+Модуль таблиц оборудования в БД.
+    EquipmentType: модель данных для хранения типа оборудования.
+    Equipment: Модель данныз для хранения сведений о оборудовани.
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class EquipmentType(models.Model):
+    """Таблица для хранения типа оборудования."""
+    name = models.CharField("Наименование", max_length=100)
+    serial_number_mask = models.CharField("Маска серийного номера",
+                                          max_length=100)
+
+    def __str__(self) -> str:
+        """Представление записи в админ-панели."""
+        return f"Type: id {self.id}, name {self.name}."
+    
+    class Meta:
+        """Представление таблицы в админ-панели."""
+        verbose_name = 'Тип оборудования'
+        verbose_name_plural = 'Типы оборудования'
+
+
+class Equipment(models.Model):
+    """Таблица для хранения оборудования."""
+    type = models.ForeignKey(EquipmentType, on_delete=models.CASCADE,
+                             verbose_name="Тип оборудования")
+    serial_number = models.CharField("Серийный номер", max_length=200)
+    notation = models.TextField("Примечание")
+
+    def __str__(self) -> str:
+        """Представление записи в админ-панели."""
+        return f"Оборудование: id {self.id},type {self.type}, serial {self.serial_number}"
+    
+    class Meta:
+        """Представление таблицы в админ-панели."""
+        verbose_name = 'Оборудование'
+        verbose_name_plural = 'Оборудование'
+        unique_together = (('type', 'serial_number'),)
